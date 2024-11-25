4b79e484eb4c1042718eb5233bebe15624a50a33
Jared Robert Andraszek <90786584+JaredAndraszek42@users.noreply.github.com>
2024-11-25 17:02:38 -0500

    Functional Search.php

--- phpScripts/search.php
+++ phpScripts/search.php
@@ -1,81 +1,79 @@
 <?php
 $host = 'localhost';
 $dbname = 'backend_db';
-$dbusername = 'root';
-$dbpassword = 'kali';
-
-echo "<h1> Testing Search </h1>";
+$dbusername = 'app_user';
+$dbpassword = 'Blue2024';
 
 // Testing the query based on GET inputs -- will change to POST for final implementation //
-$query = "SELECT * FROM table WHERE ";
+$query = "SELECT * FROM Submission_Summary WHERE ";
 $hasMultiple = false;
 
-if (isset($_GET['gene_name'])) {
+if (isset($_GET['gene_name']) && ($_GET['gene_name'] != "")) {
     $gene_name = $_GET['gene_name'];
-    $query = $query . "SubmittedGeneSymbol LIKE '%" . $gene_name . "%'";
+    $query .= "SubmittedGeneSymbol LIKE '%" . $gene_name . "%'";
     $hasMultiple = true;
 }
 
-if (isset($_GET['classification'])) {
+if (isset($_GET['classification']) && ($_GET['classification'] != "")) {
     $classification = $_GET['classification'];
 
-    if ($hasMultiple == true) {
-        $query = $query . " AND ";
+    if ($hasMultiple) {
+        $query .= " AND ";
     }
 
-    $query = $query . "ClinicalSignificance LIKE '%" . $classification . "%'";
+    $query .= "ClinicalSignificance LIKE '%" . $classification . "%'";
     $hasMultiple = true;
 }
 
-if (isset($_GET['dna_change'])) {
+if (isset($_GET['dna_change']) && ($_GET['dna_change'] != "")) {
     $dna_change = $_GET['dna_change'];
 
-    if ($hasMultiple == true) {
-        $query = $query . " AND ";
+    if ($hasMultiple) {
+        $query .= " AND ";
     }
 
-    $query = $query . "decsription LIKE '%c." . $dna_change . "%'";
+    $query .= "Description LIKE '%c." . $dna_change . "%'";
     $hasMultiple = true;
 }
 
 if (isset($_GET['lab'])) {
     $lab = $_GET['lab'];
 
-    if ($hasMultiple == true) {
-        $query = $query . " AND ";
+    if ($hasMultiple) {
+        $query .= " AND ";
     }
 
-    $query = $query . "submitter LIKE '%" . $lab . "%'";
+    $query .= "Submitter LIKE '%" . $lab . "%'";
     $hasMultiple = true;
 }
 
-if (isset($_GET['protein_change'])) {
+if (isset($_GET['protein_change']) && ($_GET['protein_change'] != "")) {
     $protein_change = $_GET['protein_change'];
 
-    if ($hasMultiple == true) {
-        $query = $query . " AND ";
+    if ($hasMultiple) {
+        $query .= " AND ";
     }
 
-    $query = $query . "protien_change LIKE '%" . $protein_change . "%'";
+    $query .= "Description LIKE '%" . $protein_change . "%'";
     $hasMultiple = true;
 }
 
-if (isset($_GET['year'])) {
+if (isset($_GET['year']) && ($_GET['year'] != "")) {
     $year = $_GET['year'];
 
-    if ($hasMultiple == true) {
-        $query = $query . " AND ";
+    if ($hasMultiple) {
+        $query .= " AND ";
     }
 
-    $query = $query . "DateLastEvaluated LIKE '%" . $year . "%'";
+    $query .= "DateLastEvaluated LIKE '%" . $year . "%'";
 }
 
-$query = $query . ";";
+$query .= ";";
 
-echo "Query is: $query <br>";
+// Create connection using mysqli
+$conn = new mysqli($host, $dbusername, $dbpassword, $dbname);
 
-$conn = new mysqli($servername, $username, $password, $dbname);
-
+// Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
@@ -105,5 +103,4 @@ if ($result->num_rows > 0) {
 }
 
 $conn->close();
-
 ?>
